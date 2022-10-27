208672ec224c54ff718eeee4f6e1e2a94d9f4de8
Phonlaphat <phonlaphat9826@gmail.com>
2022-10-27 16:26:32 +0700

    up

---
+++ app.js
@@ -0,0 +1,9 @@
+const  app = Vue.createApp({
+    data() {
+        return {
+            message: "Hello World",
+            fname: "Mark",
+            count: 0,
+        }
+    },
+}).mount("#app")
